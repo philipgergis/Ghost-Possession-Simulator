cb4cecca6c42c4108752d55f601695b47220345e
philipgergis <philipgergis@gmail.com>
2022-05-09 19:29:12 -0700

    camera done fuck this shit

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -6,11 +6,19 @@ public class CameraTrack : MonoBehaviour
 {
     [SerializeField] private Transform tracking;
 
-    [SerializeField] private Vector3 distance;
+    [SerializeField] private float height;
+
+    [SerializeField] private float offset;
+
+    [SerializeField][Range(0, 90)] private float tilt;
 
     private void FixedUpdate()
     {
-        transform.position = distance + tracking.position;
+        transform.rotation = tracking.transform.rotation;
+        transform.Rotate(new Vector3(tilt, 0, 0));
+
+        transform.position = new Vector3(-offset * tracking.transform.forward.x, height, -offset * tracking.transform.forward.z) + tracking.transform.position;
+        Debug.Log(tracking.transform.forward);
     }
 
     public void ChangeFocus(Transform newObject)
