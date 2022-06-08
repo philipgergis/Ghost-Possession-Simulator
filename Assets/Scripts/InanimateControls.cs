126e5e48673c62f818f3e06808c0c811bb3e72e3
philipgergis <philipgergis@gmail.com>
2022-06-08 03:19:16 -0700

    sad inanimate object movement
    cannot make inanimate hard to control without complex code, tried different methods but none worked well, best bet is to ignore bc too difficult

--- Assets/Scripts/InanimateControls.cs
+++ Assets/Scripts/InanimateControls.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class InanimateControls : ParentControls
 {
+    // time it takes to move again
     [SerializeField] private float maxTime;
+
+    // time remaining to move again
     private float currentTime = 0;
 
 
     protected override void MoveEntity()
     {
+        // only move if the current time has run out
         if (inControl && currentTime <= 0)
         {
             float leftRight = mainControls.Main.Move.ReadValue<Vector3>().x;
@@ -19,20 +23,13 @@ public class InanimateControls : ParentControls
 
             if (direction.magnitude >= 0.1f)
             {
-                //float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-                ////targetAngle needs to be + 90 degrees because for some reason the player model is always 90 degrees off
-                //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle + adjustmentAngle, ref turnSmoothVelocity, turnSmoothTime);
-
-                //transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                //rb.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
                 rb.AddForce(direction * speed);
                 currentTime = maxTime;
             }
         }
         else
         {
+            // subtract from current time if tried to move but cannot
             currentTime--;
         }
     }
